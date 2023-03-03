54b1122eb5f8b3912b6ce84b7b2228c433e0008b
Jean-Luc METZ <116441043+mactepluke@users.noreply.github.com>
2023-03-03 18:53:43 +0100

    Minor updates to pmbuser controller requests.

--- src/main/java/com/paymybuddy/pmb/controller/PmbUserController.java
+++ src/main/java/com/paymybuddy/pmb/controller/PmbUserController.java
@@ -62,7 +62,7 @@ public class PmbUserController {
         PmbUser pmbUser = null;
 
         if (email.isEmpty() || password.length() < 6 || password.length() > 20 || email.length() > 40 || !email.matches(EMAIL_REGEX_PATTERN)) {
-            log.error("Invalid get request: email format must be correct and max 40 characters.");
+            log.error("Invalid post request: email format must be correct and max 40 characters, and password must be between 6-20 characters.");
             status = BAD_REQUEST;
         } else {
             email = email.toLowerCase();
@@ -89,31 +89,35 @@ public class PmbUserController {
         return new ResponseEntity<>(pmbUser, status);
     }
 
-    /*@GetMapping("/findbyemail")
-    public ResponseEntity<PmbUser> find(@RequestParam String email) {
-
+    @PutMapping("/update")
+    public ResponseEntity<PmbUser> update(@RequestParam String email, @RequestParam String password) {
+//TODO écrire un premier paramètre de type enum pour le type de paramètre à updater ?
         HttpStatus status;
         PmbUser pmbUser = null;
 
-        if (email.isEmpty() || email.length() > 40 || !email.matches(EMAIL_REGEX_PATTERN)) {
-            log.error("Invalid get request: email format must be correct and max 40 characters.");
+        /*if (email.isEmpty() || password.length() < 6 || password.length() > 20 || email.length() > 40 || !email.matches(EMAIL_REGEX_PATTERN)) {
+            log.error("Invalid post request: email format must be correct and max 40 characters, and password must be between 6-20 characters.");
             status = BAD_REQUEST;
         } else {
             email = email.toLowerCase();
 
-            log.info("Get request received with email:{}", email);
+            log.info("Post request received with email:{}, password:{}", email, password.replaceAll(".", "*"));
 
-            pmbUser = pmbUserService.getUser(email);
+            pmbUser = pmbUserService.create(email, password);
 
             if (pmbUser == null) {
-                log.error("No user found with email:{}", email);
-                status = NO_CONTENT;
+                log.error("User already exists with email:{}", email);
+                status = NOT_ACCEPTABLE;
             } else {
-                log.debug("Get request successful.");
-                status = OK;
+                log.info("User created with id: {}", (pmbUser.getUserId() == null ? "<no_id>" : pmbUser.getUserId()));
+                status = CREATED;
             }
-        }
+        }*/
+        status = OK;
+        log.debug("Put request successful.");
 
         return new ResponseEntity<>(pmbUser, status);
-    }*/
+    }
+
+
 }
